bd806721c1a21c39851cc50ce6f089218b01675a
spraveenb25 <spraveenb41@gmail.com>
2026-01-01 12:28:46 -0600

    7th assignment string has been reversed

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let reversedStr = "";
+    for(i = str.length-1; i>=0; i-- ){
+        reversedStr += str[i]; 
+    }
+    return reversedStr;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
