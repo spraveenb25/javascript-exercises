c6321bd2781b96e61f4a69d712540d297f8cbe41
spraveenb25 <spraveenb41@gmail.com>
2025-12-31 21:04:13 -0600

    5th and 6th assignments are done

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str,a) {
+    sum ="";
+    if (a<0){
+        return "ERROR"
+    }
+    for (let i=0; i<a; i++){
+        sum+=str;
+    }
+    return sum;
 };
 
 // Do not edit below this line
