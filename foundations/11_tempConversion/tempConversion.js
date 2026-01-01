9e7702e5c4fd36a84aec04299bc7437f9e093b01
spraveenb25 <spraveenb41@gmail.com>
2026-01-01 17:19:56 -0600

    temp Converstion is done

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  total = (temp - 32)*100/180;
+  return Number(total.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  total = (temp*180/100)+32
+  return Number(total.toFixed(1));
 };
 
 // Do not edit below this line
