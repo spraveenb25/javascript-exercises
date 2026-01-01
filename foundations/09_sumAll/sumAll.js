d4fdfd344c792f3f5d9b23064cc4efd08ab222db
spraveenb25 <spraveenb41@gmail.com>
2026-01-01 16:51:13 -0600

    sum all assignment is done

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    if(!Number.isInteger(a) || !Number.isInteger(b)){return "ERROR"}
+    if(a<0 || b<0){return "ERROR"}
+    let sum = 0;
+    if(a<b){
+            for (i = a; i<=b; i++){ // a = 4 b = 7
+            sum = sum + i;
+        }
+    }
+    else {
+        for (i = b; i<=a; i++){ // a = 7 b = 7
+            sum = sum + i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
